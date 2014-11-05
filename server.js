0b5b47ccdea3b0f6090fcc4046ff2a4a9bde141d
Josh Lankford <lankford.josh@gmail.com>
2014-11-04 20:02:30 -0800

    Create basic server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var port       = process.env.PORT || 9000;
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+}
+
+app.use(bodyParser.json());
+app.use(allowCrossDomain);
+require('./routes/routes')(app);
+
+app.listen(port);
+console.log('server running on port: ' + port);
+exports = module.exports = app;
