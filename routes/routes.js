03015861e6c620672a2b8cd3430228ce979ca6da
Justin Pinili <pinili.justin@gmail.com>
2014-11-07 22:06:42 -0800

    (refactor) adding comments to fully understand what is happening with passport.

--- routes/routes.js
+++ routes/routes.js
@@ -19,6 +19,9 @@ passport.use(new LinkedInStrategy({
   clientSecret: 'T5nt3O8QEsZXY8vR',
   callbackURL: "http://localhost:9000/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_fullprofile'],
+
+  // callback function will be ran once authentication is successful
+
   }, function(accessToken, refreshToken, profile, done) {
     console.log('Profile: ', profile);
     process.nextTick(function () {
@@ -42,6 +45,11 @@ var getOauthToken = function(req, res, next){
   var server_token = jwt.sign({foo: 'bar'}, 'lalala');
   // console.log('Inside OAUTH - JWT: ', server_token);
   console.log('Just before redirect');
+
+  // this where we *SHOULD* store the user and corresponding oAuth tokens
+  // in the database
+
+  // is the full URL === http://localhost:9000/auth/linkedin/callback/?oauth_token=PLACEHOLDER&userId=PLACEHOLDER ?
   res.redirect('?oauth_token=' + server_token + '&userId=' + 1 );
 }
 
@@ -51,8 +59,20 @@ var getOauthToken = function(req, res, next){
   });
 
 // Linkedin route
+
+  // the route we direct users to when we want them to authenticate via linked in
+  // i'm assuming that the this returns the URL that's needed by the client to provide
+  // a way for the user to authenticate. (the URL contains a temporary oAuth token)
   app.get('/auth/linkedin', passport.authenticate('linkedin', {state: 'SOME STATE'}));
+
+  // once a user is signed, linked in will invoke the callback provided on line 20
+  // which will invoke the line below. this causes passport to send another POST request
+  // to linked in with the temporary oAuth token as well as another "secret" token. Once
+  // linked in verifies this POST request it returns the access token, refresh token, and profile.
   app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {state: 'SOME STATE'}));
+
+  // invokes the endevr created strategy to create a jwt
+  // and the jwt gets sent to the client
   app.get('/auth/linkedin/callback', getOauthToken);
 
 // Wildcard route
