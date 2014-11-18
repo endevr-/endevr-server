19fa97ed700fe8d984a9f19bdb30164ee5d70b65
Josh Lankford <lankford.josh@gmail.com>
2014-11-18 15:57:07 -0800

    edit db config for deployment

--- config/knex.js
+++ config/knex.js
@@ -1,12 +1,12 @@
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host     : '127.0.0.1',
-    user     : null,
-    password : null,
-    database : 'endevr',
-    port:      5432,
-    ssl:       false
+    host     : process.env.HEROKU_HOST || '127.0.0.1',
+    user     : process.env.HEROKU_USER || null,
+    password : process.env.HEROKU_PASS || null,
+    database : process.env.HEROKU_DB   || 'endevr',
+    port:      process.env.HEROKU_PORT || 5432,
+    ssl:       process.env.HEROKU_SSL  ||  false
   }
 });
 
