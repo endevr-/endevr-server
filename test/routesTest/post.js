faa60f8d623f1454419c1e3e307bec56f452dbb2
Justin Pinili <pinili.justin@gmail.com>
2014-11-21 20:17:01 -0800

    (feat) post request now in place for employers

--- test/routesTest/post.js
+++ test/routesTest/post.js
@@ -55,4 +55,57 @@ describe('POST', function() {
         });
     });
   });
+
+  describe('/api/employers', function() {
+
+    it('/matches should tell you if there is a match', function(done) {
+      request(endevrServer)
+        .post('/api/employers/matches'+input.empqueryParams)
+        .send({ devid: input.devid,
+                posid: input.empposid,
+                empint: true
+              })
+        .expect(function(res) {
+          body = res.body;
+        })
+        .end(function(err, res) {
+          body.match.should.equal(true);
+          done();
+        })
+    });
+
+    it('/matches should not update info when no JWT is present', function(done) {
+      request(endevrServer)
+        .post('/api/employers/matches')
+        .send({ devid: input.devid,
+                posid: input.empposid,
+                empint: true
+              })
+        .end(function(err, res) {
+          res.headers.location.should.equal('/unauthorized');
+          done();
+        });
+    });
+
+    it('/profile should update your profile info', function(done) {
+      request(endevrServer)
+        .post('/api/employers/profile'+input.empqueryParams)
+        .send({ category: 'name',
+                data: 'The Company, Inc.'
+              })
+        .expect(200, done);
+    });
+
+    it('/profile should not update info when no JWT is present', function(done) {
+      request(endevrServer)
+        .post('/api/employers/profile')
+        .send({ category: 'name',
+                data: 'The Cookie Company'
+              })
+        .end(function(err, res) {
+          res.headers.location.should.equal('/unauthorized');
+          done();
+        });
+    });
+  });
 });
